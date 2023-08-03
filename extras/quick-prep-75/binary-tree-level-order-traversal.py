052095f63c41d713363a086eda0952dfb4864ef5
Bonvic Bundi <bundi.bonvic@gmail.com>
2023-08-03 06:39:05 +0300

    explain code

--- extras/quick-prep-75/binary-tree-level-order-traversal.py
+++ extras/quick-prep-75/binary-tree-level-order-traversal.py
@@ -8,21 +8,34 @@ class TreeNode:
 
 class Solution:
     def levelOrder(self, root):
+        # we can solve this using bfs and queues
         q = deque()
+        # if there is root, append to a queue
         if root:
             q.append(root)
+        # this will store our results
         result = []
+        # while there is something in the queue, keep looping
         while q:
+            # this will store values on that level
             values = []
+            # get the length of the current level of a tree
             length = len(q)
+            # loop through the level while popping out the children
             for i in range(length):
+                # get the child
                 child = q.popleft()
+                #  add the child to curr level list
                 values.append(child.val)
+                # if the child has left child, add it to queue
                 if child.left:
                     q.append(child.left)
+                # if the child has right child, add it to queue
                 if child.right:
                     q.append(child.right)
+            # after traversing through the level add the values to the result list
             result.append(values)
+        # return the result
         return result
 
 
--- extras/quick-prep-75/combination-sum.py
+++ extras/quick-prep-75/combination-sum.py
@@ -1,15 +1,24 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # initialise a list where the combinations will be stored
         result = []
+        # create a dfs function, with the current index, curr list and current sum of the combinations
         def dfs(i, curr, total):
+            # if the current sum is equal to total, add the values into our combinations and exit
+            # we copy the curr_list because we don't wanna rewrite it once added
             if total == target:
                 result.append(curr.copy())
                 return
+            # if the total is over the candidates or index out of bound, we exit our recursion
             if i >= len(candidates) or total > target:
                 return
+            # append current index multiple times -> eg. list => [1,2,3,4] and target is 3 will be => [1,1,1]
             curr.append(candidates[i])
             dfs(i, curr, total + candidates[i])
+            # remove the current, because we don't want duplicates in other forms
             curr.pop()
+            # run the rest of the values without duplicates
             dfs(i+1, curr, total)
+        # call the dfs func
         dfs(0, [], 0)
         return result
